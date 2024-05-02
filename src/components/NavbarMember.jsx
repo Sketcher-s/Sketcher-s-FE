91642715c89db3edd31c81be3b87b326c9d7a628
Danjjinbibbang <hyjmex@gmail.com>
2024-05-03 00:11:57 +0900

    회원 navbar 수정

--- src/components/NavbarMember.jsx
+++ src/components/NavbarMember.jsx
@@ -1,20 +1,16 @@
-import React, {useState} from "react";
+import React from "react";
 import { Container, HeaderWrapper, Header, HeaderTitle, HeaderButtons, Button, Text, Divider } from './NavbarStyle';
 import { useMediaQuery } from "react-responsive";
 import { FiMenu } from "react-icons/fi";
 import PropTypes from 'prop-types';
 
-const Navbar = ({toggleSidebar}) => {
-  const isMobile = useMediaQuery({maxWidth: 767});
+const Navbar = () => {
   
   return (
     <Container>
       <HeaderWrapper>
         <Header>
           <HeaderTitle>Catch Mind</HeaderTitle>
-          {isMobile ? (
-            <FiMenu size="20" color="#5678BE" onClick={toggleSidebar}/>
-          ) : (
             <HeaderButtons>
             <Button width={52}>
               <Text>로그인</Text>
@@ -23,7 +19,6 @@ const Navbar = ({toggleSidebar}) => {
               <Text>회원가입</Text>
             </Button>
           </HeaderButtons>
-          )}
         </Header>
       </HeaderWrapper>
       <Divider />
@@ -31,9 +26,4 @@ const Navbar = ({toggleSidebar}) => {
   );
 };
 
-
 export default Navbar;
-
-Navbar.propTypes = {
-  toggleSidebar: PropTypes.bool.isRequired, // width prop이 숫자 타입이며 반드시 필요함을 명시
-};
