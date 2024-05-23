512d6d2f408274ee3386d40bc9aea90bfb8fd6ba
Danjjinbibbang <hyjmex@gmail.com>
2024-05-24 00:49:11 +0900

    app.jsx 경로 수정

--- src/app.jsx
+++ src/app.jsx
@@ -4,42 +4,37 @@ import './index.css';
 import PrepareDraw from './components/Draw/PrepareDraw';
 import Draw from './components/Draw/Draw';
 import Loading from './components/Draw/Loading';
-//import styled from 'styled-components';
 import './assets/font/font.css';
 import PreparePicture from './components/Draw/PreparePic';
-import NavbarMember from './components/NavbarMember';
-//import NavbarNoMember from './components/NavbarNoMember'
-import Sidebar from './components/Sidebar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import MyPage from './pages/MyPage';
 import Result from './pages/Result';
 import Main from './pages/Main';
 import Camera from './components/Draw/Camera';
+import Navbar from './components/Navbar';
+import { RecoilRoot } from 'recoil';
 
 function App() {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const toggleSidebar = () =>{
-        setIsSidebarOpen(!isSidebarOpen);
-    };
     return (
       <div>
-        <BrowserRouter>
-            <NavbarMember toggleSidebar={toggleSidebar}/>
-            <Sidebar isOpen={isSidebarOpen} toggle={() => setIsSidebarOpen(false)}/>
-            <Routes>
-              <Route path="/" element={<Login/>} />
-              <Route path="/register" element={<Register/>} />
-              <Route path="/mypage" element={<MyPage/>} />
-              <Route path="/preparedraw" element={<PrepareDraw/>} />
-              <Route path="/draw" element={<Draw/>} />
-              <Route path="/preparepicture" element={<PreparePicture/>} />
-              <Route path="/loading" element={<Loading/>} />
-              <Route path="/main" element={<Main/>} />
-              <Route path="/result" element={<Result/>} />
-              <Route path="/camera" element={<Camera/>} />
-            </Routes>
-          </BrowserRouter>
+        <RecoilRoot>
+          <BrowserRouter>
+              <Navbar/>
+              <Routes>
+                <Route path="/login" element={<Login/>} />
+                <Route path="/register" element={<Register/>} />
+                <Route path="/mypage" element={<MyPage/>} />
+                <Route path="/preparedraw" element={<PrepareDraw/>} />
+                <Route path="/draw" element={<Draw/>} />
+                <Route path="/preparepicture" element={<PreparePicture/>} />
+                <Route path="/loading" element={<Loading/>} />
+                <Route path="/" element={<Main/>} />
+                <Route path="/result" element={<Result/>} />
+                <Route path="/camera" element={<Camera/>} />
+              </Routes>
+            </BrowserRouter>
+        </RecoilRoot>
       </div>
     );
 
